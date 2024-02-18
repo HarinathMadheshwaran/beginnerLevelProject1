a32c2757e0a26df66b13eb8c79aac674b6366b7e
Harinath C M <125362841+HarinathMadheshwaran@users.noreply.github.com>
2024-02-18 07:29:26 +0530

    Update calculator.js

--- Project-Calculator/calculator.js
+++ Project-Calculator/calculator.js
@@ -1,34 +1,3 @@
-/*let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
-let result ="";
-
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>
-    {
-        if(e.target.innerHTML == '='){
-            result = eval(result);
-            input.value = result;
-
-        }
-
-        else if(e.target.innerHTML == 'AC'){
-            result = "";
-            result = input.value;
-
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            result = result.substring(0, result.length-1);
-           result = input.value;
-
-          
-        }
-        else{
-            result += e.target.innerHTML;
-          result = input.value;
-
-        }})
-})*/
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 let result = "";
